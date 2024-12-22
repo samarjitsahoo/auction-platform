952c0d7fc689ae2a5d30319109f1da8ee1ca0b45
Samarjit Sahoo <samarjit9203@gmail.com>
2024-12-22 18:49:30 +0530

    Refactor Code

--- frontend/src/store/slices/commissionSlice.js
+++ frontend/src/store/slices/commissionSlice.js
@@ -24,7 +24,7 @@ export const postCommissionProof = (data) => async (dispatch) => {
   dispatch(commissionSlice.actions.postCommissionProofRequest());
   try {
     const response = await axios.post(
-      "https://auction-platform-jcsw.onrender.com/api/v1/commission/proof",
+      "https://auction-platform-backend-2hjd.onrender.com/api/v1/commission/proof",
       data,
       {
         withCredentials: true,
